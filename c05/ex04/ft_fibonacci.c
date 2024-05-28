5102654a49db9a11ac9d8448fc50857748172d67
laurent cauchy <laurent.cauchy35@gmail.com>
2024-05-29 01:05:07 +0200

    ft_fibonacci.c

---
+++ c05/ex04/ft_fibonacci.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int ft_fibonacci(int index)
+{
+    if (index < 0)
+        return (-1);
+    if (index == 0)
+        return (0);
+    if (index == 1)
+        return (1);
+    
+    return (ft_fibonacci(index -1) + ft_fibonacci(index - 2));
+}
