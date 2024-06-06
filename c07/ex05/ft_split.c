b2d64a966d0021080e8be5b7fc01c8e40622ae55
laurent cauchy <laurent.cauchy35@gmail.com>
2024-06-06 18:37:05 +0200

    ft_split avec 5 fonctions max et 25 lignes max ...

--- c07/ex05/ft_split.c
+++ c07/ex05/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: lcauchy <coherence.ing@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/29 21:20:34 by lcauchy           #+#    #+#             */
-/*   Updated: 2024/05/30 04:22:55 by lcauchy          ###   ########.fr       */
+/*   Updated: 2024/06/06 18:35:27 by lcauchy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,77 +25,101 @@ int	ft_strlen(char *base)
 	return (len);
 }
 
+int found_charset(char *str, char *charset, int i_str)
+{
+    int i_set;
+    int len;
+    int i;
 
+    i_set = 0;
+    len = ft_strlen(charset);
+    i = 0;
+    while (i < len)
+    {
+        if (str[i_str + i] == charset[i_set])
+        {
+            i++;
+            i_set++;
+        }
+        else
+            break;
+    }
+    if (i == len)
+        return (1);
+    else
+        return (0);    
+}
+
+int	nombre_de_mots(char *str, char *charset)
+{
+    int nombre;
+    int i_str;
+    
+    nombre = 1;
+    i_str = 0;
+    while (str[i_str])
+    {
+        if (str[i_str] == charset[0])
+        {
+            if (found_charset(str, charset, i_str))
+                nombre++;
+        }
+        i_str++;
+    }
+    return (nombre);
+}
+
+
+char *recherche_mot(char *str, char *charset, int position)
+{
+    char    *mot;
+    int     i_str;
+    int     i_mot;
+    int     i;
+    
+    i = 0;
+    i_str = 0;
+    i_mot = 0;
+    mot = malloc(sizeof(char) * ft_strlen(str));
+    while (i < nombre_de_mots(str, charset))
+    {
+        while (found_charset(str, charset, i_str) == 0 && str[i_str])
+        {
+            mot[i_mot] = str[i_str];
+            i_str++;
+            i_mot++;
+        }
+        if (i == position)
+            return (mot);
+        i++;
+        i_str += ft_strlen(charset);
+        i_mot = 0;
+    }
+}        
+
+ 
 char **ft_split(char *str, char *charset)
 {
     char    **tab;
+    int     nb_mots;
     int     i;
-    int     j;
-    int     k;
-    int     l = 0;
-    int     len_charset;
-    int     index_tab;
-    int     index_tab_debut;
-    int     index_tab_fin;
-    int     len_mot;
-
-    tab = (char **)malloc(sizeof(char) * ft_strlen(str));
-    i = 0;
-    j = 1;
-    k = 0;
-    len_charset = ft_strlen(charset);
-    index_tab = 0;
-    index_tab_debut = 0;
-    index_tab_fin = 0;
-// recherche charset - index_tab pour malloc chaine index_tab.................................
-    if (len_charset == 0)
-        return (0);
-    while (str[i])
-    {
-        // Boucle charset .................................
-        if (str[i] == charset[0])
+    char    *mot;
+    
+    nb_mots = nombre_de_mots(str, charset);
+    tab = (char **)malloc(sizeof(char*) * nb_mots);
+    i = 0;    
+    if (nb_mots == 1)
         {
-            while (j < len_charset)
-            {
-                if (str[i+j] == charset[j])
-                    j++;
-                else
-                    j = len_charset + 1;
-            }
-            // charset found .................................
-            if (j == len_charset)
-            {
-                // affectation malloc du tab[index].........
-                index_tab_fin = i;
-                len_mot = index_tab_fin - index_tab_debut;
-                tab[index_tab] = malloc(sizeof(char) * len_mot + 1);
-                k = 0;
-                while(k < len_mot)
-                {
-                    tab[index_tab][k] = str[index_tab_debut + k];
-                    k++;
-                }
-                tab[index_tab][len_mot] = 0;
-                index_tab++;
-                i = i + len_charset - 1;
-                index_tab_debut = i + 1;
-            }
-            // charset not found .................................
-            j = 1;
+        tab[0] = malloc(sizeof(char) * ft_strlen(str));
+        tab[0] = str;
+        return (tab);
         }
+    while (i < nb_mots)
+    {
+        mot = recherche_mot(str, charset, i);
+        tab[i] = malloc(sizeof(char) * ft_strlen(mot + 1));
+        tab[i] = mot;
         i++;
     }
-    // affectation malloc du tab[dernier index].............
-    index_tab_fin = i;
-    len_mot = index_tab_fin - index_tab_debut;
-    tab[index_tab] = malloc(sizeof(char) * len_mot + 1);
-    k = 0;
-    while(k < len_mot)
-    {
-        tab[index_tab][k] = str[index_tab_debut + k];
-        k++;
-    }
-    tab[index_tab][len_mot] = 0;
-
-    return tab;
+    return (tab);
 }
